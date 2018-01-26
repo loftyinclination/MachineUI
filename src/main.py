4c1a81abc717c658419356cde748c33c90280e81
Alastair <alastaircodd@hotmail.co.uk>
2018-01-26 22:54:06 +0000

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import argparse
+from classes import Node
+
+def main(area):
+    while (_area(tree) < area))
+
+def _area(tree):
+    return [min()]
+
+if __name__ == "__main__":
+    parse = argparse.ArgumentParser(prog='Machine UI Generator')
+    parse.add_argument('-x', default=10)
+    parse.add_argument('-y', default=10)
+    parse.add_argument('-S', '--seed')
+    parse.add_argument('-b', '--branching', default=0.3)
+    parse.add_argument('-r', '--ratio', default=7)
+    arguments = parse.parse_args()
+
+    main([arguments.x, arguments.y])
