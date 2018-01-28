02ae31bbd87a82aee5c1372093bc4291fc156bef
Alastair <alastaircodd@hotmail.co.uk>
2018-01-28 13:59:58 +0000

    Add split method which lets nodes branch

--- src/classes.py
+++ src/classes.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 class Node(object):
 
@@ -14,7 +15,8 @@ class Node(object):
     def add_child(self, new_child):
         self.children.append(new_child)
 
-    def get_children(self):
+    @property
+    def children(self):
         return self.children
 
     def get_direction(self):
@@ -22,7 +24,9 @@ class Node(object):
             return None
         else:
             theta = math.atan((self.parent.y_pos - self.y_pos) / (self.parent.x_pos - self.x_pos))
-            # print(f"theta {theta}, p.x {self.parent.x_pos}, x {self.x_pos}, p.y {self.parent.y_pos}, y {self.y_pos}")
+            # print(f"theta {theta},
+            #       p.x {self.parent.x_pos}, x {self.x_pos},
+            #       p.y {self.parent.y_pos}, y {self.y_pos}")
             if (self.parent.x_pos > self.x_pos) and (self.parent.y_pos < self.y_pos):
                 return math.pi + theta
             elif (self.parent.x_pos > self.x_pos) and (self.parent.y_pos > self.y_pos):
@@ -30,3 +34,33 @@ class Node(object):
             elif (self.parent.x_pos < self.x_pos) and (self.parent.y_pos > self.y_pos):
                 return math.pi * 2 + theta
             return theta
+
+    def set_position_polar(self, norm, theta):
+        parent_theta = self.parent.get_direction()
+        if parent_theta is None:
+            parent_theta = math.pi / 4
+        self.x_pos = self.parent.x_pos + (norm * math.cos(theta + parent_theta))
+        self.y_pos = self.parent.y_pos + (norm * math.sin(theta + parent_theta))
+
+    def split(self, children, angle, angle_step, length, length_var):
+        """produce {children} new nodes, with {self} as parent
+           angle: +ve max value of angle from current direction.
+           angle_step: angle can have values of n * angle_step
+           returns new children as a list"""
+        steps = int(math.pi / angle_step)
+        angles = [n * angle_step for n in range(-steps, steps) if abs(n * angle_step) <= angle]
+        # print(angles)
+        new_children = []
+
+        for _ in range(children):
+            new_child = Node(self, None, None)
+
+            new_direction = random.choice(angles)
+            new_length = random.gauss(length, length_var)
+            angles.remove(new_direction)
+
+            new_child.set_position_polar(new_length, new_direction)
+            self.add_child(new_child)
+            new_children.append(new_child)
+
+        return new_children
--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 import argparse
-from src.classes import Node
+import random
+import math
+from classes import Node
 
-def main(area):
+def main(area, length, length_var):
     tree = [Node(None, 0, 0)]
-    tree[0]        
+
+    num_children_l_1 = random.randrange(1, 3, 1)
+    print(num_children_l_1)
+    tree += tree[0].split(num_children_l_1, math.pi / 4, math.pi / 4, length, length_var)
+    print(tree)
 
 def _area(tree):
     x_values = [i.x_pos for i in tree]
@@ -12,11 +18,15 @@ def _area(tree):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='Machine UI Generator')
-    parser.add_argument('-a', '--area', nargs=2, type=float)
-    parser.add_argument('-S', '--seed')
+    parser.add_argument('-a', '--area', nargs=2, type=float, default=[10, 10])
+    parser.add_argument('-S', '--seed', default=random.randrange(2**16))
     parser.add_argument('-b', '--branching', default=0.3)
     parser.add_argument('-r', '--ratio', default=7)
+    parser.add_argument('-l', '--length', default=1)
+    parser.add_argument('-v', '--lengthvar', default=0.6)
     cla = parser.parse_args()
 
-    print(cla.area)
-    main(area)
+    print(f"seed {cla.seed}; length {cla.length}, length variance {cla.lengthvar}")
+    random.seed(cla.seed)
+
+    main(cla.area, cla.length, cla.lengthvar)
